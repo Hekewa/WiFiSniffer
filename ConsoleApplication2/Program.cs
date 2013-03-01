2c5231466917072440a29141d6469c15e29b0508
hekewa <hekewa@hotmail.com>
2013-03-01 21:57:06 +0200

    Catch lohko ja tiedostoon kirjoitus

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 // Namespace for WlanApi.
 using NativeWifi;
 
-namespace ConsoleApplication2{
+namespace ConsoleApplication2
+{
     class Program
     {
         static string GetStringForSSID(Wlan.Dot11Ssid ssid)
@@ -16,47 +17,64 @@ namespace ConsoleApplication2{
 
         static void Main(string[] args)
         {
-            WlanClient client = new WlanClient();
-
-            while (true)
+            try
             {
-                foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
-                {
-                    Console.WriteLine(wlanIface.InterfaceDescription);
-                    Wlan.WlanBssEntry[] bssEntries = wlanIface.GetNetworkBssList();
-                     foreach (Wlan.WlanBssEntry bssEntry in bssEntries)
-                     {      
-                        Console.WriteLine(GetStringForSSID(bssEntry.dot11Ssid));
-                        //Console.WriteLine(Encoding.ASCII.GetString(bssEntry.dot11Bssid, 0, 6));
-                        byte[] macAddr = bssEntry.dot11Bssid;
-                        var macAddrLen = (uint)macAddr.Length;
-                        var str = new string[(int)macAddrLen];
-                        for (int i = 0; i < macAddrLen; i++)
+                WlanClient client = new WlanClient();
+                using (System.IO.StreamWriter outputfile = new System.IO.StreamWriter(@"C:\Users\Public\WifiSniffer.txt"))
+
+                    while (true)
+                    {
+                        foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
                         {
-                            str[i] = macAddr[i].ToString("x2");
+                            Console.WriteLine(wlanIface.InterfaceDescription);
+                            Wlan.WlanBssEntry[] bssEntries = wlanIface.GetNetworkBssList();
+                            foreach (Wlan.WlanBssEntry bssEntry in bssEntries)
+                            {
+                                Console.WriteLine(GetStringForSSID(bssEntry.dot11Ssid));
+                                //Console.WriteLine(Encoding.ASCII.GetString(bssEntry.dot11Bssid, 0, 6));
+                                byte[] macAddr = bssEntry.dot11Bssid;
+                                var macAddrLen = (uint)macAddr.Length;
+                                var str = new string[(int)macAddrLen];
+                                for (int i = 0; i < macAddrLen; i++)
+                                {
+                                    str[i] = macAddr[i].ToString("x2");
+                                }
+                                string mac = string.Join("", str);
+                                Console.WriteLine(mac);
+                                outputfile.WriteLine(mac);
+                            }
+                            Console.WriteLine("Press enter to scan again");
+                            Console.ReadLine();
+
                         }
-                        string mac = string.Join("", str);
-                        Console.WriteLine(mac);
-                     }
-                Console.WriteLine("Press enter to scan again");
-                Console.ReadLine();
+                    }
+            }
+
+            catch
+            {
+                Console.WriteLine("Error: Wlan not working properly");
+                while (true) { }
             }
         }
     }
 }
+            
+      
+    
 
-            // Wlan.WlanBssEntry[] entry = wlanIface.GetNetworkBssList();
+
+// Wlan.WlanBssEntry[] entry = wlanIface.GetNetworkBssList();
 
 
-            /*
-            foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
-            {
-                Console.WriteLine(wlanIface.InterfaceDescription);
-                Wlan.WlanAvailableNetwork[] networks = wlanIface.GetAvailableNetworkList(0);
-                foreach (Wlan.WlanAvailableNetwork network in networks)
-                {
-                    Console.WriteLine(GetStringForSSID(network.dot11Ssid)
-                        + "\t RSSI strength: {0}", network.wlanSignalQuality);
-                }
-            }
-            */
+/*
+foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
+{
+    Console.WriteLine(wlanIface.InterfaceDescription);
+    Wlan.WlanAvailableNetwork[] networks = wlanIface.GetAvailableNetworkList(0);
+    foreach (Wlan.WlanAvailableNetwork network in networks)
+    {
+        Console.WriteLine(GetStringForSSID(network.dot11Ssid)
+            + "\t RSSI strength: {0}", network.wlanSignalQuality);
+    }
+}
+*/
